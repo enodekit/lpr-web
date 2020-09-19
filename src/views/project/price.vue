b02fc9716abb8a1f76df4acd0bfe465c05569f37
sakurahanafantasy <admin@qq.com>
2020-09-19 21:38:44 +0800

    12312

--- src/views/project/price.vue
+++ src/views/project/price.vue
@@ -1,137 +1,57 @@
 <template>
   <!-- START: Portfolio -->
   <div style="background-color:#f2f3f7">
-    <div class="container" style="margin:0 15%;padding:16px 0px">
-      <div class="thumb-example" style="width: 35%;float:left">
-        <!-- swiper1 -->
-        <swiper ref="swiperTop" class="swiper gallery-top" :options="swiperOptionTop">
-          <swiper-slide v-for="slide in swiperSlides" :key="slide" :style="slide">
-            <!--<div slot="button-next" class="swiper-button-next swiper-button-white" />-->
-            <!--<div slot="button-prev" class="swiper-button-prev swiper-button-white" />-->
-          </swiper-slide></swiper>
-        <!-- swiper2 Thumbs -->
-        <swiper ref="swiperThumbs" class="swiper gallery-thumbs" :options="swiperOptionThumbs">
-          <swiper-slide v-for="slide in swiperSlides" :key="slide" :style="slide" />
-        </swiper>
-      </div>
-      <div style="width: 65%;float: left">
-        <div class="ma-title-wrap">
-          <h1 style="display: inline;font-size: 16px;color: #333;line-height: 24px" title="Ruiqu Party Decoration Love Letter Cake Toppers Acrylic Gold Happy Birthday Cake Topper" data-spm-anchor-id="a2700.details.maonnacta.i0.412e5d27RrZddL">Ruiqu Party Decoration Love Letter Cake Toppers Acrylic Gold Happy Birthday Cake Topper</h1>
-          <span id="detail-title-tags-after-title" class="promotion-title-tag pt-after" />
-          <div class="ma-tag-wrap">
-            <span id="detail-title-tags-below-title" class="promotion-tags" />
-            <div class="ma-tag ma-tag-orange hot-sale-tag hot-sale-tag-1" style="display: none">
-              Hot sale products
-            </div>
-            <div class="ma-tag ma-tag-orange hot-sale-tag hot-sale-tag-2" style="display: none">
-              Hot sale in <span />
-            </div>
+    <div class="container">
+      <div style="background-color:#ffffff;border-top:#e6e7eb solid 1px;">
+        <div class="content-header">
+          <div class="detail-subscribe">
+            <el-breadcrumb separator-class="el-icon-arrow-right" class="detail-breadcrumb">
+              <el-breadcrumb-item :to="{ path: '/' }">Home</el-breadcrumb-item>
+              <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+              <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+              <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+            </el-breadcrumb>
           </div>
         </div>
-        <div class="key-attrs" />
-        <div class="ma-main">
-          <div class="ma-fob-title">FOB <b>Reference</b> Price:<a id="J-get-last-price" class="ma-glp" data-domdot="id:3303,ext:is_xinbao_product=1|is_display_assurance=1" href="javascript:;" rel="nofollow">Get Latest Price</a></div>
-          <div id="detail-banner" class="promotion-banner" />
-          <div class="ma-price-wrap">
-            <div class="ma-reference-price">
-              <span class="ma-ref-price">
-                <span>$0.10 - $0.20</span>
-              </span>
-              / Piece
-              <span class="ma-split">|</span>
-              <span class="ma-min-order">100 Piece/Pieces</span> (Min. Order)
-            </div>
+        <div class="content-body">
+          <div class="thumb-example" style="float:left;width:380px">
+            <!-- swiper1 -->
+            <swiper ref="swiperTop" class="swiper gallery-top" :options="swiperOptionTop">
+              <swiper-slide v-for="slide in swiperSlides" :key="slide" :style="slide" />
+            </swiper>
+            <!-- swiper2 Thumbs -->
+            <swiper ref="swiperThumbs" class="swiper gallery-thumbs" :options="swiperOptionThumbs">
+              <swiper-slide v-for="slide in swiperSlides" :key="slide" :style="slide" />
+            </swiper>
           </div>
-          <div id="detail-coupon" class="detail-coupon" /><div id="detail-coupon-banner" class="promotion-coupon-banner" />
-          <div class="ma-brief-list">
-            <div id="skuWrap">
-              <dl class="sku-attr-dl util-clearfix COLOR" data-role="sku-attr-item" data-id="191288010">
-                <dt class="name" title="Color">Color:</dt>
-                <dd class="sku-attr-val value util-clearfix">
-                  <span class="sku-attr-val-frame picture-frame" data-role="sku-attr-val" data-id="3331116">
-                    <span class="color" style="background-color: #FFDC00;" title="Gold" />
-                    <span class="icon-sku-attr-val" />
-                  </span>
-                </dd>
-              </dl>
-            </div>
-            <dl class="do-entry-item">
-              <dt class="do-entry-item-key" title="Lead Time">
-                Lead Time:
-              </dt>
-              <dd class="do-entry-item-val" title="Lead Time">
-                <table class="supply-ability-table ui2-table ui2-table-normal ui2-table-zebra ui2-table-hover">
-                  <tbody>
-                    <tr>
-                      <td>Quantity(Pieces)</td>
-                      <td>1 - 10000</td>
-                      <td>10000</td>
-                    </tr>
-                    <tr>
-                      <td>Est. Time(days)</td>
-                      <td>5</td>
-                      <td>Negotiable</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </dd>
-            </dl>
-            <dl class="customize customize-list util-clearfix">
-              <dt class="name">Customization:</dt>
-              <dd class="value">
-                <div class="customization-list-item">
-                  Customized logo
-                  <span class="customization-list-item-moq">(Min. Order: 10000 Pieces)</span>
+          <div style="width: 65%;float: left">
+            <div class="ma-title-wrap">
+              <h1 data-spm-anchor-id="a2700.details.maonnacta.i0.412e5d27RrZddL">
+                {{ project.name }}
+              </h1>
+              <span id="detail-title-tags-after-title" class="promotion-title-tag pt-after" />
+              <div class="ma-tag-wrap">
+                <span id="detail-title-tags-below-title" class="promotion-tags" />
+                <div class="ma-tag ma-tag-orange hot-sale-tag hot-sale-tag-1">
+                  Hot sale products
                 </div>
-                <div class="customization-list-item">
-                  Customized packaging
-                  <span class="customization-list-item-moq">(Min. Order: 10000 Pieces)</span>
+                <div class="ma-tag ma-tag-orange hot-sale-tag hot-sale-tag-2">
+                  Hot sale in
                 </div>
-              </dd>
-            </dl>
-          </div>
-        </div>
-        <div class="ma-operate">
-          <!-- aff:false, ta: true, fc:true -->
-          <div class="ma-main-operate">
-            <!-- actionTestVersion: VB action2TestVersion: CLOSE-->
-            <a class="ui2-button ui2-button-primary ui2-button-large ma-button-contact-supplier" data-domdot="id:3301,ext:'is_xinbao_product=1|is_display_assurance=1'" href="//message.alibaba.com/msgsend/contact.htm?action=contact_action&amp;domain=1&amp;id=62035120895&amp;id_f=IDX1Jpetgs7QJk0_bPGLC5fRAJmBA8SX0qAMaTWmeF-qbGoGyi3RNkVfdne4wVq89vOe&amp;mloca=main_en_detail&amp;tracelog=tracedetailfeedback&amp;umidToken=Bbe7beeb7a4632e51cb9d88c6f0739569" title="Click to send an inquiry" rel="nofollow" target="_blank" data-spm="dmessage"><i class="ui2-icon ui2-icon-left ui2-icon-message" /> Contact Supplier </a>
-          </div>
-          <div class="ma-sub-operate">
-            <a
-              href="javascript:;"
-              data-domdot="id:3302,ext:'is_xinbao_product=1|is_display_assurance=1'"
-              data-role="alitalk"
-              class="atm link-default atm-online"
-              data-uid="8pctgRBMALObQL85sWQxG+idvsomQx97"
-              data-pid="62035120895"
-              data-contactid="8pctgRBMALObQL85sWQxG+idvsomQx97"
-              data-biztype="1"
-              data-contactname="Ms. Eda"
-              data-contactimg="//sc01.alicdn.com/kf/HTB1dakVPirpK1RjSZFhq6xSdXXaM.jpg_80x80.jpg"
-              data-productid="62035120895"
-              data-bizid="62035120895"
-              data-is-new-webatm="true"
-              data-scenetype="detail"
-              data-widget-cid="widget-2"
-            >
-              <i data-role="atm-icon" class="ui2-icon ui2-icon-alitalk" />
-              <span data-role="atm-text" title="Chat with me">Chat Now</span>
-            </a>
-
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    <el-tabs v-model="activeName" type="card" style="margin:0 15%" @tab-click="handleClick">
-      <el-tab-pane label="Product Details" name="pd" style="background-color: #ffffff;">
+    <el-tabs v-model="activeName" type="card" class="tab-style">
+      <el-tab-pane label="Product Details" name="pd" style="background-color: #ffffff;padding-left:10px!important;">
         <div v-html="project.content" />
       </el-tab-pane>
-      <el-tab-pane label="Company Profile" name="cp" style="background-color: #ffffff">
+      <el-tab-pane label="Company Profile" name="cp" style="background-color: #ffffff;padding-left:10px!important;">
         <div v-html="project.company" />
       </el-tab-pane>
     </el-tabs>
-    <!-- END: Portfolio -->
   </div>
 </template>
 
@@ -170,7 +90,10 @@ export default {
         status: true,
         isshow: true
       },
-      project: undefined,
+      project: {
+        content: undefined,
+        company: undefined
+      },
       swiperSlides: [],
       activeName: 'pd'
     }
@@ -189,7 +112,6 @@ export default {
   methods: {
     async getProductClass() {
       this.project = (await getOne(this.$route.params.id)).data
-      console.log(this.project)
       this.swiperSlides = []
       this.project.imgs.forEach(item => {
         this.swiperSlides.push('background-image:url("' + item + '")')
@@ -200,9 +122,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .thumb-example {
-    background-color: #f6f6f6;
-  }
   .swiper {
   .swiper-slide {
     background-size: cover;
@@ -212,12 +131,14 @@ export default {
   &.gallery-top {
      height: 350px;
      width: 350px;
+     margin: 0 auto;
+     border: 1px solid #e6e7eb;
    }
   &.gallery-thumbs {
      height: 90px;
      width: 350px;
      box-sizing: border-box;
-     padding: 10px 0;
+     padding-top: 10px;
    }
   &.gallery-thumbs .swiper-slide {
      width: 70px;
@@ -226,6 +147,8 @@ export default {
    }
   &.gallery-thumbs .swiper-slide-active {
      opacity: 1;
+     width: 68px;
+     height: 68px;
      border: 2px solid #fe9b0a;
    }
     &.gallery-thumbs .swiper-slide-active .arrow{
@@ -245,5 +168,65 @@ export default {
     border-top: 1px solid #e6e7eb;
     background-color: #fff;
     padding: 0 18px;
+  }
+  .content-body {
+    position: relative;
+    box-sizing: content-box;
+    width: 100%;
+    max-width: 1200px;
+    padding: 0 16px;
+    margin: 0 auto;
+  }
+  .tab-style{
+    margin: 0 auto;
+    max-width: 1200px;
+    padding-top:20px;
+    padding-right: 240px;
+    background-color: transparent;
+  }
+  .ma-title-wrap h1{
+    display: inline;
+    font-size: 16px;
+    color: #333;
+    line-height: 24px
+  }
+  .widget-main-action .ma-tag-orange{
+    background-color: #edf0f3;
+    color: #999;
+  }
+  .widget-main-action .ma-tag {
+    display: inline-block;
+    vertical-align: top;
+    margin-right: 4px;
+    font-weight: 400;
+    background-color: #e8f1fa;
+    color: #6b8eb3;
+    line-height: 22px;
+    height: 20px;
+    font-size: 12px;
+    padding: 0 8px;
+    position: relative;
+    zoom: 1;
+  }
+  .detail-subscribe {
+    max-width: 1200px;
+    margin: 7px auto;
+    position: relative;
+    z-index: 1;
+  }
+  .detail-subscribe .detail-breadcrumb {
+    display: inline-block;
+    vertical-align: top;
+    height: 26px;
+    overflow: hidden;
+    font-size: 12px;
+    line-height: 26px;
+  }
+  .detail-subscribe .detail-breadcrumb .breadcrumb-link {
+    font-size: 12px;
+    line-height: 26px;
+  }
+  .detail-subscribe .detail-breadcrumb .seperator {
+    margin: 0 5px;
   }
 </style>
